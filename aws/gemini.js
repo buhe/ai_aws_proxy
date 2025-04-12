94fd6131a9e7105adcf98e1fda2d5a876c438782
buhe <bugu1986@126.com>
2025-04-12 16:57:36 +0800

    "Add AWS Gemini and AWS Grok Lambda functions for handling HTTP requests and responses."

---
+++ aws/gemini.js
@@ -0,0 +1,53 @@
+import https from 'https'
+
+export const handler = awslambda.streamifyResponse(
+    async (event, responseStream, _context) => {
+        const headers = event.headers;
+        const body = event.body;
+        const path = event.rawPath;
+        const httpMethod = event.requestContext.http.method;
+        console.log(`path: ${path}`)
+        console.log(`httpMethod: ${JSON.stringify(httpMethod)}`)
+        const options = {
+            method: httpMethod,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': headers['authorization']
+            },
+            port: 443,
+            hostname: 'generativelanguage.googleapis.com',
+            path: path,
+        }
+        const metadata = {
+            statusCode: 200,
+            headers: {
+                "Content-Type": "application/json",
+                "CustomHeader": "outerspace"
+            }
+        };
+
+        // Assign to the responseStream parameter to prevent accidental reuse of the non-wrapped stream.
+        responseStream = awslambda.HttpResponseStream.from(responseStream, metadata);
+
+        return new Promise((resolve, reject) => {
+            const req = https.request(options, (res) => {
+                res.on('data', (chunk) => {
+                    responseStream.write(chunk)
+                });
+                res.on('end', () => {
+                    responseStream.end();
+                    responseStream.finished();
+                    resolve();
+                });
+            });
+
+            req.on('error', (e) => {
+                console.error(`problem with request: ${e.message}`);
+                responseStream.end();
+                responseStream.finished();
+                reject(e);
+            });
+            req.write(body)
+            req.end();
+        });
+    })
